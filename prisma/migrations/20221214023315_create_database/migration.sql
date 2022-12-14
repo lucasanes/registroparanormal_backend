a482eb48755e86eefdead9cc119275dc7c37cb2e
Lightz-11 <lucasanes007@outlook.com>
2022-12-13 23:34:35 -0300

    up v0.1

---
+++ prisma/migrations/20221214023315_create_database/migration.sql
@@ -0,0 +1,348 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessoes" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "sessoes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "participantes" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "sessaoId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "fichaId" TEXT NOT NULL,
+
+    CONSTRAINT "participantes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "convites" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "sessaoId" TEXT NOT NULL,
+    "userEmail" TEXT NOT NULL,
+
+    CONSTRAINT "convites_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "anotacoes" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "sessaoId" TEXT NOT NULL,
+
+    CONSTRAINT "anotacoes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "iniciativas" (
+    "id" TEXT NOT NULL,
+    "posicao" INTEGER NOT NULL,
+    "nome" TEXT NOT NULL,
+    "iniciativa" INTEGER NOT NULL,
+    "dano" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "sessaoId" TEXT NOT NULL,
+
+    CONSTRAINT "iniciativas_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "dados" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "valor" TEXT NOT NULL,
+    "isDano" BOOLEAN NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "fichaId" TEXT,
+    "sessaoId" TEXT,
+
+    CONSTRAINT "dados_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "itens" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT,
+    "espaco" INTEGER NOT NULL,
+    "categoria" INTEGER NOT NULL,
+    "imagem" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "sessaoId" TEXT,
+    "fichaId" TEXT,
+
+    CONSTRAINT "itens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "armas" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "tipo" TEXT NOT NULL,
+    "ataque" TEXT NOT NULL,
+    "dano" TEXT NOT NULL,
+    "margemCritico" INTEGER NOT NULL,
+    "danoCritico" TEXT NOT NULL,
+    "recarga" TEXT,
+    "alcance" TEXT NOT NULL,
+    "especial" TEXT,
+    "descricao" TEXT,
+    "espaco" INTEGER NOT NULL,
+    "categoria" INTEGER NOT NULL,
+    "imagem" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "sessaoId" TEXT,
+    "fichaId" TEXT,
+
+    CONSTRAINT "armas_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "fichas" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    "sessaoId" TEXT,
+
+    CONSTRAINT "fichas_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "fichasPrincipal" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "jogador" TEXT NOT NULL,
+    "classe" TEXT NOT NULL,
+    "origem" TEXT NOT NULL,
+    "nacionalidade" TEXT NOT NULL,
+    "peprod" INTEGER NOT NULL,
+    "idade" INTEGER NOT NULL,
+    "idadeAdicional" INTEGER,
+    "deslocamento" INTEGER NOT NULL,
+    "nex" INTEGER NOT NULL,
+    "trilha" TEXT,
+    "patente" TEXT NOT NULL,
+    "fichaId" TEXT NOT NULL,
+
+    CONSTRAINT "fichasPrincipal_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "fichasAtributo" (
+    "id" TEXT NOT NULL,
+    "agi" INTEGER NOT NULL,
+    "int" INTEGER NOT NULL,
+    "vig" INTEGER NOT NULL,
+    "pre" INTEGER NOT NULL,
+    "for" INTEGER NOT NULL,
+    "fichaId" TEXT NOT NULL,
+
+    CONSTRAINT "fichasAtributo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "fichasStatus" (
+    "id" TEXT NOT NULL,
+    "combate" BOOLEAN NOT NULL,
+    "insano" BOOLEAN NOT NULL,
+    "danoMassivo" BOOLEAN NOT NULL,
+    "inconsciente" BOOLEAN NOT NULL,
+    "pv" INTEGER NOT NULL,
+    "pvMax" INTEGER NOT NULL,
+    "ps" INTEGER NOT NULL,
+    "psMax" INTEGER NOT NULL,
+    "pe" INTEGER NOT NULL,
+    "peMax" INTEGER NOT NULL,
+    "municao" INTEGER,
+    "municaoMax" INTEGER,
+    "peso" INTEGER NOT NULL,
+    "fichaId" TEXT NOT NULL,
+
+    CONSTRAINT "fichasStatus_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "fichasPericias" (
+    "id" TEXT NOT NULL,
+    "acrobacia" INTEGER,
+    "adestramento" INTEGER,
+    "arte" INTEGER,
+    "atletismo" INTEGER,
+    "atualidade" INTEGER,
+    "ciencia" INTEGER,
+    "crime" INTEGER,
+    "diplomacia" INTEGER,
+    "enganacao" INTEGER,
+    "fortitude" INTEGER,
+    "furtividade" INTEGER,
+    "iniciativa" INTEGER,
+    "intimidacao" INTEGER,
+    "intuicao" INTEGER,
+    "investigacao" INTEGER,
+    "luta" INTEGER,
+    "medicina" INTEGER,
+    "ocultismo" INTEGER,
+    "percepcao" INTEGER,
+    "pilotagem" INTEGER,
+    "pontaria" INTEGER,
+    "profissao" INTEGER,
+    "reflexo" INTEGER,
+    "religiao" INTEGER,
+    "sobrevivencia" INTEGER,
+    "tatica" INTEGER,
+    "tecnologia" INTEGER,
+    "vontade" INTEGER,
+    "fichaId" TEXT NOT NULL,
+
+    CONSTRAINT "fichasPericias_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "fichasDefesas" (
+    "id" TEXT NOT NULL,
+    "passiva" INTEGER,
+    "esquiva" INTEGER,
+    "bloqueio" INTEGER,
+    "mental" INTEGER,
+    "morte" INTEGER,
+    "conhecimento" INTEGER,
+    "sangue" INTEGER,
+    "energia" INTEGER,
+    "fisica" INTEGER,
+    "balistica" INTEGER,
+    "corte" INTEGER,
+    "impacto" INTEGER,
+    "perfuracao" INTEGER,
+    "eletricidade" INTEGER,
+    "fogo" INTEGER,
+    "frio" INTEGER,
+    "quimica" INTEGER,
+    "fichaId" TEXT NOT NULL,
+
+    CONSTRAINT "fichasDefesas_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "fichasPersonagem" (
+    "id" TEXT NOT NULL,
+    "historia" TEXT,
+    "aparencia" TEXT,
+    "pep" TEXT,
+    "dfm" TEXT,
+    "favoritos" TEXT,
+    "personalidade" TEXT,
+    "piorpesadelo" TEXT,
+    "anotacoesPlayer" TEXT,
+    "anotacoesPersonagem" TEXT,
+    "fichaId" TEXT NOT NULL,
+
+    CONSTRAINT "fichasPersonagem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "fichasPortrait" (
+    "id" TEXT NOT NULL,
+    "normal" TEXT,
+    "ferido" TEXT,
+    "insano" TEXT,
+    "insanoeferido" TEXT,
+    "morrendo" TEXT,
+    "fichaId" TEXT NOT NULL,
+
+    CONSTRAINT "fichasPortrait_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "sessoes" ADD CONSTRAINT "sessoes_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "participantes" ADD CONSTRAINT "participantes_sessaoId_fkey" FOREIGN KEY ("sessaoId") REFERENCES "sessoes"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "participantes" ADD CONSTRAINT "participantes_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "participantes" ADD CONSTRAINT "participantes_fichaId_fkey" FOREIGN KEY ("fichaId") REFERENCES "fichas"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "convites" ADD CONSTRAINT "convites_sessaoId_fkey" FOREIGN KEY ("sessaoId") REFERENCES "sessoes"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "convites" ADD CONSTRAINT "convites_userEmail_fkey" FOREIGN KEY ("userEmail") REFERENCES "users"("email") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "anotacoes" ADD CONSTRAINT "anotacoes_sessaoId_fkey" FOREIGN KEY ("sessaoId") REFERENCES "sessoes"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "iniciativas" ADD CONSTRAINT "iniciativas_sessaoId_fkey" FOREIGN KEY ("sessaoId") REFERENCES "sessoes"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "dados" ADD CONSTRAINT "dados_fichaId_fkey" FOREIGN KEY ("fichaId") REFERENCES "fichas"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "dados" ADD CONSTRAINT "dados_sessaoId_fkey" FOREIGN KEY ("sessaoId") REFERENCES "sessoes"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "itens" ADD CONSTRAINT "itens_sessaoId_fkey" FOREIGN KEY ("sessaoId") REFERENCES "sessoes"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "itens" ADD CONSTRAINT "itens_fichaId_fkey" FOREIGN KEY ("fichaId") REFERENCES "fichas"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "armas" ADD CONSTRAINT "armas_sessaoId_fkey" FOREIGN KEY ("sessaoId") REFERENCES "sessoes"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "armas" ADD CONSTRAINT "armas_fichaId_fkey" FOREIGN KEY ("fichaId") REFERENCES "fichas"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "fichas" ADD CONSTRAINT "fichas_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "fichas" ADD CONSTRAINT "fichas_sessaoId_fkey" FOREIGN KEY ("sessaoId") REFERENCES "sessoes"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "fichasPrincipal" ADD CONSTRAINT "fichasPrincipal_fichaId_fkey" FOREIGN KEY ("fichaId") REFERENCES "fichas"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "fichasAtributo" ADD CONSTRAINT "fichasAtributo_fichaId_fkey" FOREIGN KEY ("fichaId") REFERENCES "fichas"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "fichasStatus" ADD CONSTRAINT "fichasStatus_fichaId_fkey" FOREIGN KEY ("fichaId") REFERENCES "fichas"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "fichasPericias" ADD CONSTRAINT "fichasPericias_fichaId_fkey" FOREIGN KEY ("fichaId") REFERENCES "fichas"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "fichasDefesas" ADD CONSTRAINT "fichasDefesas_fichaId_fkey" FOREIGN KEY ("fichaId") REFERENCES "fichas"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "fichasPersonagem" ADD CONSTRAINT "fichasPersonagem_fichaId_fkey" FOREIGN KEY ("fichaId") REFERENCES "fichas"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "fichasPortrait" ADD CONSTRAINT "fichasPortrait_fichaId_fkey" FOREIGN KEY ("fichaId") REFERENCES "fichas"("id") ON DELETE CASCADE ON UPDATE CASCADE;
