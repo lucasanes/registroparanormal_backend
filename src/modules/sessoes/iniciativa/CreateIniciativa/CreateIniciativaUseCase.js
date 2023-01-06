1867f4a63ee9b652ef7e389ce52199e8d63a7372
Lightz-11 <lucasanes007@outlook.com>
2023-01-05 23:59:53 -0300

    c

--- src/modules/sessoes/iniciativa/CreateIniciativa/CreateIniciativaUseCase.js
+++ src/modules/sessoes/iniciativa/CreateIniciativa/CreateIniciativaUseCase.js
@@ -3,23 +3,14 @@ const prisma = require("../../../database/prisma");
 
 class CreateIniciativaUseCase {
   async execute({ nome, iniciativa, dano, sessaoId }) {
+
     const ultimaPosicao = await prisma.iniciativa.count();
 
     if (ultimaPosicao == 10) {
       throw new AppError("Você não pode adicionar mais do que 10 iniciativas.")
-    } 
+    }
 
     const posicao = ultimaPosicao + 1;
-
-    const sessaoIdAlreadyExists = await prisma.sessao.findFirst({
-      where: {
-        id: sessaoId,
-      },
-    });
-
-    if (!sessaoIdAlreadyExists) {
-      throw new AppError("Não existe nenhuma sessão com o ID passado.");
-    }
 
     const data = await prisma.iniciativa.create({
       data: {
