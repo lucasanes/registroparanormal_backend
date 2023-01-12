67280808a8fa2a548168e24f7cb8daccbfb0811c
Lightz-11 <lucasanes007@outlook.com>
2023-01-12 11:09:42 -0300

    sos

--- src/modules/sessoes/sessao/GetSessaoById/GetSessaoByIdUseCase.js
+++ src/modules/sessoes/sessao/GetSessaoById/GetSessaoByIdUseCase.js
@@ -30,9 +30,6 @@ class GetSessaoByIdUseCase {
             Pericias: true,
             Portrait: true,
             Defesas: true
-          },
-          orderBy: {
-            nome: 'asc'
           }
         },
         FichasNPC: {
