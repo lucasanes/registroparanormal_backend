a3dcc6e4d273afcd47ddf0bf125cc4e14b7b6163
SkyLuke11 <sky.lucas003@gmail.com>
2023-10-26 20:16:33 -0300

    a

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use((error, request, response, next) => {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       status: error.statusCode,
-      mensagem: error.mensagem
+      mensagem: error.msg
     });
   }
   return response.status(500).json({
