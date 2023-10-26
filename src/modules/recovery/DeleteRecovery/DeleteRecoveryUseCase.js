ea709ed29226940fae5b17cdac7e87c66691b44d
SkyLuke11 <sky.lucas003@gmail.com>
2023-10-26 18:56:49 -0300

    aaaaaa

--- src/modules/recovery/DeleteRecovery/DeleteRecoveryUseCase.js
+++ src/modules/recovery/DeleteRecovery/DeleteRecoveryUseCase.js
@@ -1,6 +1,5 @@
 const prisma = require("../../database/prisma");
 const AppError = require("../../../utils/AppError");
-require('../../../globalFunctions')
 
 class DeleteRecoveryUseCase {
   async execute({email}) {
