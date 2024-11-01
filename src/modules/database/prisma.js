7766806ccf180fa1a73c848f61492baf4f0a7b5e
Lucas (Lightz) <sky.lucas003@gmail.com>
2024-11-01 19:18:51 -0300

    fix: teste

--- src/modules/database/prisma.js
+++ src/modules/database/prisma.js
@@ -1,5 +1,31 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+// .$extends({
+//   query: {
+//     $allOperations: async (args, query) => {
+//       if (args.args && args.args.where) {
+//         for (const key in args.args.where) {
+//           if (key.includes('id') && typeof args.args.where[key] === 'string') {
+//             args.args.where[key] = parseInt(args.args.where[key], 10);
+//           }
+//         }
+//       }
+
+//       if (args.args && args.args.data) {
+//         for (const key in args.args.data) {
+//           if (key.includes('id') && typeof args.args.data[key] === 'string') {
+//             args.args.data[key] = parseInt(args.args.data[key], 10);
+//           }
+//         }
+//       }
+
+//       console.log(args);
+
+//       const result = await query(args);
+//       return result;
+//     },
+//   },
+// });
 
 prisma.$use(async (params, next) => {
   if (params.args && params.args.where) {
