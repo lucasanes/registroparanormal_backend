c9450e644890b3ed7bb1855f4fe0f0c2a90f2444
Lucas (Lightz) <sky.lucas003@gmail.com>
2024-11-01 20:06:03 -0300

    fix: change includes to endWith id

--- src/modules/database/prisma.js
+++ src/modules/database/prisma.js
@@ -19,8 +19,6 @@ const prisma = new PrismaClient();
 //         }
 //       }
 
-//       console.log(args);
-
 //       const result = await query(args);
 //       return result;
 //     },
@@ -31,22 +29,22 @@ prisma.$use(async (params, next) => {
   const convertIdsInWhere = (obj) => {
     if (obj) {
       for (const key in obj) {
-        if (key.toLowerCase().includes('id') && typeof obj[key] === 'string') {
-          obj[key] = parseInt(obj[key], 10);
+        if (key.toLowerCase().endsWith('id') && typeof obj[key] === 'string') {
+          obj[key] = Number(obj[key]);
         } else if (typeof obj[key] === 'object' && obj[key] !== null) {
           for (const key2 in obj[key]) {
             if (
-              key2.toLowerCase().includes('id') &&
+              key2.toLowerCase().endsWith('id') &&
               typeof obj[key][key2] === 'string'
             ) {
-              obj[key][key2] = parseInt(obj[key][key2], 10);
+              obj[key][key2] = Number(obj[key][key2]);
             } else if (typeof obj[key][key2] === 'object' && obj[key][key2]) {
               for (const key3 in obj[key][key2]) {
                 if (
-                  key3.toLowerCase().includes('id') &&
+                  key3.toLowerCase().endsWith('id') &&
                   typeof obj[key][key2][key3] === 'string'
                 ) {
-                  obj[key][key2][key3] = parseInt(obj[key][key2][key3], 10);
+                  obj[key][key2][key3] = Number(obj[key][key2][key3]);
                 }
               }
             }
@@ -59,22 +57,22 @@ prisma.$use(async (params, next) => {
   const convertIdsInData = (obj) => {
     if (obj) {
       for (const key in obj) {
-        if (key.toLowerCase().includes('id') && typeof obj[key] === 'string') {
-          obj[key] = parseInt(obj[key], 10);
+        if (key.toLowerCase().endsWith('id') && typeof obj[key] === 'string') {
+          obj[key] = Number(obj[key]);
         } else if (typeof obj[key] === 'object' && obj[key] !== null) {
           for (const key2 in obj[key]) {
             if (
-              key2.toLowerCase().includes('id') &&
+              key2.toLowerCase().endsWith('id') &&
               typeof obj[key][key2] === 'string'
             ) {
-              obj[key][key2] = parseInt(obj[key][key2], 10);
+              obj[key][key2] = Number(obj[key][key2]);
             } else if (typeof obj[key][key2] === 'object' && obj[key][key2]) {
               for (const key3 in obj[key][key2]) {
                 if (
-                  key3.toLowerCase().includes('id') &&
+                  key3.toLowerCase().endsWith('id') &&
                   typeof obj[key][key2][key3] === 'string'
                 ) {
-                  obj[key][key2][key3] = parseInt(obj[key][key2][key3], 10);
+                  obj[key][key2][key3] = Number(obj[key][key2][key3]);
                 }
               }
             }
