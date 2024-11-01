1400447d70b555b3404a1598314d50d481a20d1d
Lucas (Lightz) <sky.lucas003@gmail.com>
2024-11-01 18:55:07 -0300

    feat: add middleware for id's in prisma

--- src/modules/database/prisma.js
+++ src/modules/database/prisma.js
@@ -1,4 +1,25 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+
+prisma.$use(async (params, next) => {
+  if (params.args && params.args.where) {
+    for (const key in params.args.where) {
+      if (key.includes('id') && typeof params.args.where[key] === 'string') {
+        params.args.where[key] = parseInt(params.args.where[key], 10);
+      }
+    }
+  }
+
+  if (params.args && params.args.data) {
+    for (const key in params.args.data) {
+      if (key.includes('id') && typeof params.args.data[key] === 'string') {
+        params.args.data[key] = parseInt(params.args.data[key], 10);
+      }
+    }
+  }
+
+  const result = await next(params);
+  return result;
+});
 
 module.exports = prisma;
